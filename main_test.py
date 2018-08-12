96ebb86acd8242ba29afd858fde6a43e4c660192
martin grossman <martin.grossman@gmail.com>
2018-08-12 14:31:17 +0200

    added pipelined plot

--- main_test.py
+++ main_test.py
@@ -3,6 +3,7 @@ from HELPERS import helper_funs as hf
 #importing some useful packages
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
+from collections import OrderedDict
 
 
 #read data
@@ -52,3 +53,55 @@ hf.plot_imfiles(test_images_lst,images_gb,'gray','aaaa')
 
 
 plt.show()
+
+
+
+# ad image processing filters to create ordered pipeline as ordered dict
+# for each filter add parameters
+filters_funs_d=OrderedDict(); #needs to be ordered to fix the order (no need in 3.6 up)
+filters_funs_d[hf.grayscale] = 'none'
+filters_funs_d[hf.gaussian_blur] = (9,) #needs to be tuple
+filters_funs_d[hf.canny] = (100,200)
+
+
+# for f,v in filters_funs_d.items():
+#     print(f,v)
+#     print(type(v))
+#     if v == '':
+#         print('empty')
+
+
+def plot_improcsess(image_file, img_name ='', filters_funs_lst=[], fgs=(20, 10)):
+
+    cmap=None
+    img= mpimg.imread(image_file)
+    cols= len(filters_funs_lst)+1
+
+    plt.figure(figsize=fgs)
+
+    plt.subplot(1, cols, 1)
+    plt.title(img_name)
+    plt.imshow(img)
+    image_temp=img
+
+
+    for idx,(f,v) in enumerate(filters_funs_d.items()):
+        # funs
+        print(image_temp.shape)
+        #print(idx,f)
+        plt.subplot(1, cols, idx+2)
+        plt.title(f.__name__)
+        cmap = 'gray' if len(image_temp.shape) == 2 else cmap
+
+
+        if v == 'none':
+            image_temp = f(image_temp)
+            plt.imshow(image_temp,cmap=cmap)
+        else:
+            #print(v)
+            #print(type(v))
+            args=v
+            image_temp = f(image_temp,*args)
+            plt.imshow(image_temp,cmap=cmap)
+
+plot_improcsess(test_images_fullpath_list[0],test_images_lst[0],filters_funs_d)
