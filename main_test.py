15b9f71d6dad2d45fec0c25008d0d07f37650845
martin grossman <martin.grossman@gmail.com>
2018-08-16 00:16:54 +0200

    added numpy region

--- main_test.py
+++ main_test.py
@@ -1,28 +1,18 @@
 from HELPERS import helper_funs as hf
 
-#importing some useful packages
-import matplotlib.pyplot as plt
+# importing some useful packages
 import matplotlib.image as mpimg
 from collections import OrderedDict
+import cv2
+import numpy as np
 
-
-#read data
+# read data
 test_images_dir = 'test_images/'
 test_videos_dir = 'test_videos'
-test_images_lst = hf.dir_content_lst(test_images_dir)
 test_images_fullpath_list = hf.dir_content_fullpath_lst(test_images_dir)
 
-#read images via imread
-#convert to grayscale
-#edge detect
-images_im = [] # originals list
-images_gs = [] # grayscaled list
-images_ed = [] # edges list
-images_gb = [] # gaussian blur list
-images_ms = [] # masked images
 
-
-#parameters
+# parameters
 canny_low = 100
 canny_high = 200
 gbsize = 7
@@ -34,79 +24,36 @@ if (gbsize%2==0):
     gbsize = gbsize + 1
 
 
-def process_filters(image_in, image_name_in, filters_dict):
-    # process image in by applying filters in filters_dict
-    image_temp = image_in
-    image_process_pipe_lst = [(image_in, image_name_in)]
-    # if no filtering funs
-    if filters_dict:
-        for idx, (f, param) in enumerate(filters_dict.items()):
-            if param == 'none':
-                image_temp = f(image_temp)
-                image_process_pipe_lst.append((image_temp, f.__name__))
-            else:
-                args = param
-                image_temp = f(image_temp, *args)
-                image_process_pipe_lst.append((image_temp, f.__name__))
-
-    return image_process_pipe_lst
 
 
-def plot_filters(image_pipe_lst, fgs=(20, 10)):
-    # plot filter for one image
-    colormap = None
-    cols = len(image_pipe_lst)
-    plt.figure(figsize=fgs)
-
-    for idx, img_tuple in enumerate(image_pipe_lst):
-        if len(img_tuple[0].shape) == 2:
-            colormap = 'gray'
-        plt.subplot(1, cols, idx + 1)
-        plt.title(img_tuple[1])
-        plt.imshow(img_tuple[0], cmap=colormap)
-    plt.show()
-
-
-def plot_pipes(processed_images_lst, fgs=(20, 15)):
-    # plots filters on all images in processed_images_lst
-    rows = len(processed_images_lst)
-    cols = len(processed_images_lst[0])
-    plt.figure(figsize=fgs)
-    colormap = None
-    for jx,image_pipe_lst in enumerate(processed_images_lst):
-        for idx, img_tuple in enumerate(image_pipe_lst):
-            if len(img_tuple[0].shape) == 2:
-                colormap = 'gray'
-            plt.subplot(rows, cols, (idx + 1)+(jx*4))
-            plt.title(img_tuple[1])
-            plt.imshow(img_tuple[0], cmap=colormap)
-    plt.tight_layout()
-    plt.show()
 
 
 # ad image processing filters to create ordered pipeline as ordered dict
 # for each filter add parameters
 filters_funs_d = OrderedDict() #needs to be ordered to fix the order (no need in 3.6 up)
+filters_funs_d[hf.select_white_yellow_L] ='none'
 filters_funs_d[hf.grayscale] = 'none'
 filters_funs_d[hf.gaussian_blur] = (9,) #needs to be tuple ()
 filters_funs_d[hf.canny] = (100,200)
+filters_funs_d[hf.region_of_interest] = 'none'
 
 # create image list
 image_list = []
 image_name_list = []
-for idx,image_file in enumerate(test_images_fullpath_list):
+for idx, image_file in enumerate(test_images_fullpath_list):
 
     image_list.append(mpimg.imread(test_images_fullpath_list[idx]))
     image_name_list.append(str(test_images_fullpath_list[idx]).split('/')[1])
 
-
+# Processing
 processed_images_lst=[]
 for image,image_name in zip(image_list, image_name_list):
-    processed_images_lst.append(process_filters(image,image_name,filters_funs_d))
+    processed_images_lst.append(hf.process_filters(image, image_name, filters_funs_d))
+
 
 
 print(len(processed_images_lst))
 print(len(processed_images_lst[0]))
 
 
-plot_pipes(processed_images_lst, fgs=(20, 10))
+hf.plot_pipes(processed_images_lst, fgs=(20, 10))
